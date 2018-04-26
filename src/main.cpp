c2017a1634b984064eb1704cfe3da8f2d2199d6d
Ankit Patel <ankitpatel96@gmail.com>
2018-04-26 13:19:17 -0700

    camera up and running

--- src/main.cpp
+++ src/main.cpp
@@ -18,10 +18,15 @@
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+// camera
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
 
 GLFWwindow *window = nullptr;
 
@@ -30,11 +35,6 @@ using namespace std;
 static void error_callback(int error, const char* description)
 {
     fprintf(stderr, "Error: %s\n", description);
-}
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
 
 void mouse_callback(GLFWwindow* window, double xpos, double ypos)
@@ -55,6 +55,37 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
   camera.ProcessMouseMovement(xoffset, yoffset);
 }
 
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+  // make sure the viewport matches the new window dimensions; note that width and
+  // height will be significantly larger than specified on retina displays.
+  glViewport(0, 0, width, height);
+}
+
+
+void processInput(GLFWwindow *window)
+{
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+
+  if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    camera.ProcessKeyboard(FORWARD, deltaTime);
+  if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    camera.ProcessKeyboard(BACKWARD, deltaTime);
+  if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    camera.ProcessKeyboard(LEFT, deltaTime);
+  if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+  camera.ProcessMouseScroll(yoffset);
+}
 
 
 void loadOpenGl() {
@@ -94,10 +125,21 @@ void loadOpenGl() {
   glfwSwapBuffers(window);
 }
 
+void setGlfwCallbacks() {
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+  glfwSetCursorPosCallback(window, mouse_callback);
+  glfwSetScrollCallback(window, scroll_callback);
+}
+
 int main(void)
 {
 
   loadOpenGl();
+
+  setGlfwCallbacks();
+
+  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
 
   // NOTE: OpenGL error checks have been omitted for brevity
   printf("OpenGL %d.%d\n", GLVersion.major, GLVersion.minor);
@@ -150,7 +192,7 @@ int main(void)
     }
   }
 
-  Shader baseshader("../src/shaders/simple.vs", "../src/shaders/simple.fs");
+  Shader baseshader("../src/shaders/camera.vs", "../src/shaders/simple.fs");
 //  int num_particles = int(dim_x) * int(dim_y) * int(dim_z) + 1;
 //  MatrixXd vertices(num_particles, 3);
 //  int z = 0;
@@ -200,14 +242,33 @@ int main(void)
 
   while (!glfwWindowShouldClose(window))
     {
+      // per-frame time logic
+      // --------------------
+      float currentFrame = glfwGetTime();
+      deltaTime = currentFrame - lastFrame;
+      lastFrame = currentFrame;
+
+      processInput(window);
+
       glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
       glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
       baseshader.use();
+
+      // pass projection matrix to shader (note that in this case it could change every frame)
+      glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+      baseshader.setMat4("projection", projection);
+
+      // camera/view transformation
+      glm::mat4 view = camera.GetViewMatrix();
+      baseshader.setMat4("view", view);
 
 
       glBindVertexArray(VAO);
       glDrawArrays(GL_TRIANGLES, 0, 4);
       snowsim->drawContents();
+
+
 
       glfwPollEvents();
 
