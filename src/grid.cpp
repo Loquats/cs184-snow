63105e733adf465f39944ca54bfbaff6f9cad111
Andy Zhang <y.zhang@berkeley.edu>
2018-04-30 21:30:16 -0700

    memoize computation of neighborhood bounds and b-spline and b-spline gradient values per simulation cycle

--- src/grid.cpp
+++ src/grid.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include "grid.h"
 #include "particle.h"
-#include "interpolation.h"
 #include "force.h"
 #include <glm/glm.hpp>
 #include <Eigen/SVD>
@@ -30,6 +29,8 @@ void Grid::resetGrid() {
     ivec3 index = glm::floor(particle->position); 
     nodes[index.x][index.y][index.z]->particles.push_back(particle);
     particle->velocity = vec3(0.0);
+    particle->compute_neighborhood_bounds(dim_x, dim_y, dim_z, h);
+    particle->compute_b_spline_grad(h);
   }
 }
 
@@ -64,14 +65,8 @@ void Grid::simulate(float delta_t, vector<vec3> external_accelerations, vector<C
     for (vec3 acc : external_accelerations) {
       total_acc += acc;
     }
-    for (int i = 0; i < dim_x; ++i) {
-        for (int j = 0; j < dim_y; ++j) {
-            for (int k = 0; k < dim_z; ++k) {
-                for (Particle* particle : nodes[i][j][k]->particles) {
-                    particle->velocity += total_acc * delta_t;
-                }
-            }
-        }
+    for (Particle *particle : all_particles) {
+      particle->velocity += total_acc * delta_t;
     }
 
     // Step 9.
@@ -86,50 +81,18 @@ void Grid::simulate(float delta_t, vector<vec3> external_accelerations, vector<C
 * in its neighborhood
 */
 void Grid::particle_to_grid() {
-  for (int i = 0; i < dim_x; ++i) {
-    for (int j = 0; j < dim_y; ++j) {
-      for (int k = 0; k < dim_z; ++k) {
-        for (Particle *particle : nodes[i][j][k]->particles) {   
-          vec3 pos = particle->position;
-          int i_lo = std::max((int) ceil(pos.x / h - 2), 0);
-          int i_hi = std::min((int) floor(pos.x / h + 2) + 1, (int) dim_x);
-          int j_lo = std::max((int) ceil(pos.y / h - 2), 0);
-          int j_hi = std::min((int) floor(pos.y / h + 2) + 1, (int) dim_y);
-          int k_lo = std::max((int) ceil(pos.z / h - 2), 0);
-          int k_hi = std::min((int) floor(pos.z / h + 2) + 1, (int) dim_z);
-          for (int dest_i = i_lo; dest_i < i_hi; ++dest_i) {
-            for (int dest_j = j_lo; dest_j < j_hi; ++dest_j) {
-              for (int dest_k = k_lo; dest_k < k_hi; ++dest_k) {
-                float weight = b_spline(pos, vec3(dest_i, dest_j, dest_k), h);
-                nodes[dest_i][dest_j][dest_k]->mass += weight * particle->mass;
-                nodes[dest_i][dest_j][dest_k]->velocity += weight * particle->mass * particle->velocity;
-              }
-            }
-          }
+  for (Particle *particle : all_particles) {
+    vec3 pos = particle->position;
+    for (int dest_i = particle->i_lo; dest_i < particle->i_hi; ++dest_i) {
+      for (int dest_j = particle->j_lo; dest_j < particle->j_hi; ++dest_j) {
+        for (int dest_k = particle->k_lo; dest_k < particle->k_hi; ++dest_k) {
+          float weight = particle->b_spline_at(dest_i, dest_j, dest_k);
+          nodes[dest_i][dest_j][dest_k]->mass += weight * particle->mass;
+          nodes[dest_i][dest_j][dest_k]->velocity += weight * particle->mass * particle->velocity;
         }
       }
     }
   }
-
-
-//    for (Particle* particle : all_particles) {
-//      vec3 pos = particle->position;
-//      int i_lo = std::max((int) ceil(pos.x / h - 2), 0);
-//      int i_hi = std::min((int) floor(pos.x / h + 2) + 1, (int) dim_x);
-//      int j_lo = std::max((int) ceil(pos.y / h - 2), 0);
-//      int j_hi = std::min((int) floor(pos.y / h + 2) + 1, (int) dim_y);
-//      int k_lo = std::max((int) ceil(pos.z / h - 2), 0);
-//      int k_hi = std::min((int) floor(pos.z / h + 2) + 1, (int) dim_z);
-//      for (int dest_i = i_lo; dest_i < i_hi; ++dest_i) {
-//        for (int dest_j = j_lo; dest_j < j_hi; ++dest_j) {
-//          for (int dest_k = k_lo; dest_k < k_hi; ++dest_k) {
-//            float weight = b_spline(pos, vec3(dest_i, dest_j, dest_k), h);
-//            nodes[dest_i][dest_j][dest_k]->mass += weight * particle->mass;
-//            nodes[dest_i][dest_j][dest_k]->velocity += weight * particle->mass * particle->velocity;
-//          }
-//        }
-//      }
-//    }
 
   // Normalize grid velocity by its mass all at once
   for (int i = 0; i < dim_x; ++i) {
@@ -148,32 +111,20 @@ void Grid::particle_to_grid() {
 */
 void Grid::compute_particle_volumes() {
   float h3 = pow(h, 3);
-  for (int i = 0; i < dim_x; ++i) {
-    for (int j = 0; j < dim_y; ++j) {
-      for (int k = 0; k < dim_z; ++k) {
-        for (Particle* particle : nodes[i][j][k]->particles) {
-          vec3 pos = particle->position;
-          int i_lo = std::max((int) ceil(pos.x / h - 2), 0);
-          int i_hi = std::min((int) floor(pos.x / h + 2) + 1, (int) dim_x);
-          int j_lo = std::max((int) ceil(pos.y / h - 2), 0);
-          int j_hi = std::min((int) floor(pos.y / h + 2) + 1, (int) dim_y);
-          int k_lo = std::max((int) ceil(pos.z / h - 2), 0);
-          int k_hi = std::min((int) floor(pos.z / h + 2) + 1, (int) dim_z);
+  for (Particle *particle : all_particles) {
+    vec3 pos = particle->position;
 
-          float density = 0;
-          for (int dest_i = i_lo; dest_i < i_hi; ++dest_i) {
-            for (int dest_j = j_lo; dest_j < j_hi; ++dest_j) {
-              for (int dest_k = k_lo; dest_k < k_hi; ++dest_k) {
-                float weight = b_spline(pos, vec3(dest_i, dest_j, dest_k), h);
-                density += weight * nodes[dest_i][dest_j][dest_k]->mass;
-              }
-            }
-          }
-          density /= h3;
-          particle->volume = particle->mass / density;
+    float density = 0;
+    for (int dest_i = particle->i_lo; dest_i < particle->i_hi; ++dest_i) {
+      for (int dest_j = particle->j_lo; dest_j < particle->j_hi; ++dest_j) {
+        for (int dest_k = particle->k_lo; dest_k < particle->k_hi; ++dest_k) {
+          float weight = particle->b_spline_at(dest_i, dest_j, dest_k);
+          density += weight * nodes[dest_i][dest_j][dest_k]->mass;
         }
       }
     }
+    density /= h3;
+    particle->volume = particle->mass / density;
   }
 }
 
@@ -184,33 +135,21 @@ void Grid::compute_particle_volumes() {
 */
 void Grid::compute_F_hat_Ep(float delta_t) {
   float h3 = pow(h, 3);
-  for (int i = 0; i < dim_x; ++i) {
-    for (int j = 0; j < dim_y; ++j) {
-      for (int k = 0; k < dim_z; ++k) {
-        for (Particle* particle : nodes[i][j][k]->particles) {
-          vec3 pos = particle->position;
-          int i_lo = std::max((int) ceil(pos.x / h - 2), 0);
-          int i_hi = std::min((int) floor(pos.x / h + 2) + 1, (int) dim_x);
-          int j_lo = std::max((int) ceil(pos.y / h - 2), 0);
-          int j_hi = std::min((int) floor(pos.y / h + 2) + 1, (int) dim_y);
-          int k_lo = std::max((int) ceil(pos.z / h - 2), 0);
-          int k_hi = std::min((int) floor(pos.z / h + 2) + 1, (int) dim_z);
+  for (Particle *particle : all_particles) {
+    vec3 pos = particle->position;
 
-          mat3 sum = mat3(0.0f);
-          for (int dest_i = i_lo; dest_i < i_hi; ++dest_i) {
-            for (int dest_j = j_lo; dest_j < j_hi; ++dest_j) {
-              for (int dest_k = k_lo; dest_k < k_hi; ++dest_k) {
-                vec3 weight_grad = b_spline_grad(pos, vec3(dest_i, dest_j, dest_k), h);
-                vec3 velocity = nodes[dest_i][dest_j][dest_k]->velocity;
-                sum += delta_t * outerProduct(velocity, weight_grad);
-              }
-            }
-          }
-          mat3 identity = mat3(1.0f);
-          particle->F_hat_Ep = (identity + sum) * particle->deformation_grad_P;
+    mat3 sum = mat3(0.0f);
+    for (int dest_i = particle->i_lo; dest_i < particle->i_hi; ++dest_i) {
+      for (int dest_j = particle->j_lo; dest_j < particle->j_hi; ++dest_j) {
+        for (int dest_k = particle->k_lo; dest_k < particle->k_hi; ++dest_k) {
+          vec3 weight_grad = particle->b_spline_grad_at(dest_i, dest_j, dest_k);
+          vec3 velocity = nodes[dest_i][dest_j][dest_k]->velocity;
+          sum += delta_t * outerProduct(velocity, weight_grad);
         }
       }
     }
+    mat3 identity = mat3(1.0f);
+    particle->F_hat_Ep = (identity + sum) * particle->deformation_grad_P;
   }
 }
 
@@ -219,31 +158,19 @@ void Grid::compute_F_hat_Ep(float delta_t) {
 */
 void Grid::compute_grid_forces(float mu_0, float lambda_0, float xi) {
   float h3 = pow(h, 3);
-  for (int i = 0; i < dim_x; ++i) {
-    for (int j = 0; j < dim_y; ++j) {
-      for (int k = 0; k < dim_z; ++k) {
-        for (Particle* particle : nodes[i][j][k]->particles) {
-          vec3 pos = particle->position;
-          int i_lo = std::max((int) ceil(pos.x / h - 2), 0);
-          int i_hi = std::min((int) floor(pos.x / h + 2) + 1, (int) dim_x);
-          int j_lo = std::max((int) ceil(pos.y / h - 2), 0);
-          int j_hi = std::min((int) floor(pos.y / h + 2) + 1, (int) dim_y);
-          int k_lo = std::max((int) ceil(pos.z / h - 2), 0);
-          int k_hi = std::min((int) floor(pos.z / h + 2) + 1, (int) dim_z);
+  for (Particle *particle : all_particles) {
+    vec3 pos = particle->position;
 
-          float volume = particle->volume;
-          mat3 sigma_p = psi_derivative(mu_0, lambda_0, xi, particle) * transpose(particle->deformation_grad_E);
+    float volume = particle->volume;
+    mat3 sigma_p = psi_derivative(mu_0, lambda_0, xi, particle) * transpose(particle->deformation_grad_E);
 
-          mat3 neg_force_unweighted = volume * sigma_p;
+    mat3 neg_force_unweighted = volume * sigma_p;
 
-          for (int dest_i = i_lo; dest_i < i_hi; ++dest_i) {
-            for (int dest_j = j_lo; dest_j < j_hi; ++dest_j) {
-              for (int dest_k = k_lo; dest_k < k_hi; ++dest_k) {
-                vec3 weight_grad = b_spline_grad(pos, vec3(dest_i, dest_j, dest_k), h);
-                nodes[dest_i][dest_j][dest_k]->force -= neg_force_unweighted * weight_grad;
-              }
-            }
-          }
+    for (int dest_i = particle->i_lo; dest_i < particle->i_hi; ++dest_i) {
+      for (int dest_j = particle->j_lo; dest_j < particle->j_hi; ++dest_j) {
+        for (int dest_k = particle->k_lo; dest_k < particle->k_hi; ++dest_k) {
+          vec3 weight_grad = particle->b_spline_grad_at(dest_i, dest_j, dest_k);
+          nodes[dest_i][dest_j][dest_k]->force -= neg_force_unweighted * weight_grad;
         }
       }
     }
@@ -308,55 +235,43 @@ void Grid::compute_time_integration() {
 * Step 7: update deformation gradients for each particle
 */
 void Grid::update_deformation_gradients(float theta_c, float theta_s, float delta_t) {
-  for (int i = 0; i < dim_x; ++i) {
-    for (int j = 0; j < dim_y; ++j) {
-      for (int k = 0; k < dim_z; ++k) {
-        for (Particle* particle : nodes[i][j][k]->particles) {
-          vec3 pos = particle->position;
-          int i_lo = std::max((int) ceil(pos.x / h - 2), 0);
-          int i_hi = std::min((int) floor(pos.x / h + 2) + 1, (int) dim_x);
-          int j_lo = std::max((int) ceil(pos.y / h - 2), 0);
-          int j_hi = std::min((int) floor(pos.y / h + 2) + 1, (int) dim_y);
-          int k_lo = std::max((int) ceil(pos.z / h - 2), 0);
-          int k_hi = std::min((int) floor(pos.z / h + 2) + 1, (int) dim_z);
+  for (Particle *particle : all_particles) {
+    vec3 pos = particle->position;
 
-          mat3 grad_vp = mat3(0.0f);
+    mat3 grad_vp = mat3(0.0f);
 
-          for (int dest_i = i_lo; dest_i < i_hi; ++dest_i) {
-            for (int dest_j = j_lo; dest_j < j_hi; ++dest_j) {
-              for (int dest_k = k_lo; dest_k < k_hi; ++dest_k) {
-                vec3 weight_grad = b_spline_grad(pos, vec3(dest_i, dest_j, dest_k), h);
-                vec3 velocity = nodes[dest_i][dest_j][dest_k]->next_velocity;
-                grad_vp += outerProduct(velocity, weight_grad);
-              }
-            }
-          }
-          mat3 identity = mat3(1.0f);
-          mat3 dgrad_E_next = (identity + delta_t * grad_vp) * particle->deformation_grad_E;
-          mat3 F_next = dgrad_E_next * particle->deformation_grad_P;
-
-          Matrix3f dgrad_E_eigen = glm_to_eigen(dgrad_E_next);
-          JacobiSVD<MatrixXf> svd(dgrad_E_eigen, ComputeFullU | ComputeFullV);
-          Matrix3f U_eigen = svd.matrixU();
-          Matrix3f V_eigen = svd.matrixV();
-          Vector3f S_eigen = svd.singularValues();
-          mat3 U = eigen_to_glm(U_eigen);
-          mat3 V = eigen_to_glm(V_eigen);
-          vec3 S_hat_vec = eigen_to_glm(S_eigen);
-
-          vec3 S_vec = clamp(S_hat_vec, 1-theta_c, 1+theta_s);
-          mat3 S = mat3(S_vec[0], 0.0, 0.0,
-                                  0.0, S_vec[1], 0.0,
-                                  0.0, 0.0, S_vec[2]);
-          mat3 S_inv = mat3(1.0/S_vec[0], 0.0, 0.0,
-                                  0.0, 1.0/S_vec[1], 0.0,
-                                  0.0, 0.0, 1.0/S_vec[2]);
-
-          particle->deformation_grad_E = U * S * transpose(V);
-          particle->deformation_grad_P = V * S_inv * transpose(U) * F_next;
+    for (int dest_i = particle->i_lo; dest_i < particle->i_hi; ++dest_i) {
+      for (int dest_j = particle->j_lo; dest_j < particle->j_hi; ++dest_j) {
+        for (int dest_k = particle->k_lo; dest_k < particle->k_hi; ++dest_k) {
+          vec3 weight_grad = particle->b_spline_grad_at(dest_i, dest_j, dest_k);
+          vec3 velocity = nodes[dest_i][dest_j][dest_k]->next_velocity;
+          grad_vp += outerProduct(velocity, weight_grad);
         }
       }
     }
+    mat3 identity = mat3(1.0f);
+    mat3 dgrad_E_next = (identity + delta_t * grad_vp) * particle->deformation_grad_E;
+    mat3 F_next = dgrad_E_next * particle->deformation_grad_P;
+
+    Matrix3f dgrad_E_eigen = glm_to_eigen(dgrad_E_next);
+    JacobiSVD<MatrixXf> svd(dgrad_E_eigen, ComputeFullU | ComputeFullV);
+    Matrix3f U_eigen = svd.matrixU();
+    Matrix3f V_eigen = svd.matrixV();
+    Vector3f S_eigen = svd.singularValues();
+    mat3 U = eigen_to_glm(U_eigen);
+    mat3 V = eigen_to_glm(V_eigen);
+    vec3 S_hat_vec = eigen_to_glm(S_eigen);
+
+    vec3 S_vec = clamp(S_hat_vec, 1-theta_c, 1+theta_s);
+    mat3 S = mat3(S_vec[0], 0.0, 0.0,
+                            0.0, S_vec[1], 0.0,
+                            0.0, 0.0, S_vec[2]);
+    mat3 S_inv = mat3(1.0/S_vec[0], 0.0, 0.0,
+                            0.0, 1.0/S_vec[1], 0.0,
+                            0.0, 0.0, 1.0/S_vec[2]);
+
+    particle->deformation_grad_E = U * S * transpose(V);
+    particle->deformation_grad_P = V * S_inv * transpose(U) * F_next;
   }
 }
 
@@ -364,35 +279,23 @@ void Grid::update_deformation_gradients(float theta_c, float theta_s, float delt
  * Step 8: Update particle velocities using PIC and FLIP parts
  */
 void Grid::update_particle_velocities(float alpha) {
-  for (int i = 0; i < dim_x; ++i) {
-    for (int j = 0; j < dim_y; ++j) {
-      for (int k = 0; k < dim_z; ++k) {
-        for (Particle* particle : nodes[i][j][k]->particles) {
-          vec3 pos = particle->position;
-          int i_lo = std::max((int) ceil(pos.x / h - 2), 0);
-          int i_hi = std::min((int) floor(pos.x / h + 2) + 1, (int) dim_x);
-          int j_lo = std::max((int) ceil(pos.y / h - 2), 0);
-          int j_hi = std::min((int) floor(pos.y / h + 2) + 1, (int) dim_y);
-          int k_lo = std::max((int) ceil(pos.z / h - 2), 0);
-          int k_hi = std::min((int) floor(pos.z / h + 2) + 1, (int) dim_z);
+  for (Particle *particle : all_particles) {
+    vec3 pos = particle->position;
 
-          vec3 v_pic = vec3(0.0);
-          vec3 v_flip = particle->velocity;
+    vec3 v_pic = vec3(0.0);
+    vec3 v_flip = particle->velocity;
 
-          for (int dest_i = i_lo; dest_i < i_hi; ++dest_i) {
-            for (int dest_j = j_lo; dest_j < j_hi; ++dest_j) {
-              for (int dest_k = k_lo; dest_k < k_hi; ++dest_k) {
-                GridNode *dest = nodes[dest_i][dest_j][dest_k];
-                float weight = b_spline(pos, vec3(dest_i, dest_j, dest_k), h);
-                v_pic += dest->next_velocity * weight;
-                v_flip += (dest->next_velocity - dest->velocity) * weight;
-              }
-            }
-          }
-          particle->velocity += (1.f - alpha) * v_pic + alpha * v_flip;
+    for (int dest_i = particle->i_lo; dest_i < particle->i_hi; ++dest_i) {
+      for (int dest_j = particle->j_lo; dest_j < particle->j_hi; ++dest_j) {
+        for (int dest_k = particle->k_lo; dest_k < particle->k_hi; ++dest_k) {
+          GridNode *dest = nodes[dest_i][dest_j][dest_k];
+          float weight = particle->b_spline_at(dest_i, dest_j, dest_k);
+          v_pic += dest->next_velocity * weight;
+          v_flip += (dest->next_velocity - dest->velocity) * weight;
         }
       }
     }
+    particle->velocity += (1.f - alpha) * v_pic + alpha * v_flip;
   }
 }
 
@@ -400,16 +303,10 @@ void Grid::update_particle_velocities(float alpha) {
  * Step 9: Particle-based collisions
  */
 void Grid::compute_particle_collisions(float delta_t, vector<CollisionObject *> *objects) {
-  for (int i = 0; i < dim_x; ++i) {
-    for (int j = 0; j < dim_y; ++j) {
-      for (int k = 0; k < dim_z; ++k) {
-        for (Particle* particle : nodes[i][j][k]->particles) {
-          for (CollisionObject *co : *objects) {
-            vec3 next_position = particle->position + delta_t * particle->velocity;
-            particle->velocity = co->collide(particle->position, next_position, particle->velocity);
-          }
-        }
-      }
+  for (Particle *particle : all_particles) {
+    for (CollisionObject *co : *objects) {
+      vec3 next_position = particle->position + delta_t * particle->velocity;
+      particle->velocity = co->collide(particle->position, next_position, particle->velocity);
     }
   }
 }
@@ -418,14 +315,8 @@ void Grid::compute_particle_collisions(float delta_t, vector<CollisionObject *> 
  * Step 10: Update particle positions
  */
 void Grid::update_particle_positions(float delta_t) {
-  for (int i = 0; i < dim_x; ++i) {
-    for (int j = 0; j < dim_y; ++j) {
-      for (int k = 0; k < dim_z; ++k) {
-        for (Particle* particle : nodes[i][j][k]->particles) {
-          particle->position += particle->velocity * delta_t;
-        }
-      }
-    }
+  for (Particle *particle : all_particles) {
+    particle->position += particle->velocity * delta_t;
   }
 }
 
---
+++ src/interpolation.cpp
@@ -0,0 +1,53 @@
+#include <glm/glm.hpp>
+#include "interpolation.h"
+
+/* sign function: https://stackoverflow.com/questions/1903954/is-there-a-standard-sign-function-signum-sgn-in-c-c
+*/ 
+float sgn(float x) {
+	return (x > 0) - (x < 0);
+}
+
+float N_func(float x) {
+	float abs_x = abs(x);
+	if (abs_x < 1) {
+		return 0.5 * pow(abs_x, 3) - pow(x, 2) + 2.0 / 3.0;
+	} else if (abs_x < 2) {
+		return -1.0 / 6.0 * pow(abs_x, 3) + pow(x, 2) - 2.0 * abs_x + 4.0 / 3.0;
+	} else {
+		return 0;
+	}
+}
+
+float N_func_derivative(float x) {
+	float abs_x = abs(x);
+	if (abs_x < 1) {
+		return 1.5 * x * x * sgn(x) - 2 * x;
+	} else if (abs_x < 2) {
+		return -0.5 * x * x * sgn(x) + 2 * x - 2 * sgn(x);
+	} else {
+		return 0;
+	}
+}
+
+float b_spline(glm::vec3 particle_pos, glm::vec3 grid_index, float h) {
+	// ie. particle_pos / h - grid_index
+	glm::vec3 scaled = (particle_pos - h * grid_index) / h;
+	return N_func(scaled.x) * N_func(scaled.y) * N_func(scaled.z);
+}
+
+glm::vec3 b_spline_components(glm::vec3 particle_pos, glm::vec3 grid_index, float h) {
+	glm::vec3 scaled = (particle_pos - h * grid_index) / h;
+	return glm::vec3(N_func(scaled.x), N_func(scaled.y), N_func(scaled.z));
+}
+
+/*
+* Using that motherfuckin chain rule, ye
+*/
+glm::vec3 b_spline_grad(glm::vec3 particle_pos, glm::vec3 grid_index, float h) {
+	glm::vec3 scaled = (particle_pos - h * grid_index) / h;
+	glm::vec3 components = b_spline_components(particle_pos, grid_index, h);
+	float dx = components.y * components.z * N_func_derivative(scaled.x) / h;
+	float dy = components.x * components.z * N_func_derivative(scaled.y) / h;
+	float dz = components.x * components.y * N_func_derivative(scaled.z) / h;
+	return glm::vec3(dx, dy , dz);
+}
